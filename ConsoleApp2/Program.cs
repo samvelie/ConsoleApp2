58594ab0746ce774d1bee40c53853bbdeede9d5e
samvelie <sam.velie@gmail.com>
2017-06-15 20:38:45 -0500

    working with Visual Studio 2017

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        //this below is a "method"
+        static void Main(string[] args)
+        {
+            var numberOfBananas = 3;
+            Console.WriteLine(numberOfBananas);
+        }
+    }
+}
